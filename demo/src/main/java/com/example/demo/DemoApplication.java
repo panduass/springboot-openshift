6a2cff28b6dbe6ab301ca007d755780a5ea0f76d
panduass <pandualanss@gmail.com>
2025-08-08 08:58:50 +0700

    first commit

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello this is quickstart application!");
+	}
+
+	@RestController
+	public class HelloController {
+		@GetMapping("/")
+		public String hello() {
+			return "Hello, Springboot with Openshift!";
+		}
+	}
+
+}
